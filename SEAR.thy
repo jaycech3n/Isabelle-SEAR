b53e81bf92f1db1b83c19c0ca464da5be89f3bdc
Josh Chen <jaycech3n@gmail.com>
2019-05-06 00:26:30 +0200

    injectivity and surjectivity; subsets

--- SEAR.thy
+++ SEAR.thy
@@ -130,9 +130,18 @@ syntax "_some_set" :: "[set, o] \<Rightarrow> set" ("(\<epsilon>set _ | _)")
 translations "\<epsilon>set X | P" \<rightleftharpoons> "CONST some_set (\<lambda>X. P)"
 
 
+subsubsection \<open>Injectivity and surjectivity\<close>
+
+definition rel_inj :: "[rel, set, set] \<Rightarrow> o" ("(_ : _ \<succ> _ injective)")
+  where "\<phi>: A \<succ> B injective \<equiv> \<forall>a \<in> A. \<forall>a' \<in> A. (\<exists>b \<in> B. \<phi>(a, b) \<and> \<phi>(a', b)) \<longrightarrow> a = a'"
+
+definition rel_surj :: "[rel, set, set] \<Rightarrow> o" ("(_ : _ \<succ> _ surjective)")
+  where "\<phi>: A \<succ> B surjective \<equiv> \<forall>b \<in> B. \<exists>a \<in> A. \<phi>(a, b)"
+
+
 subsubsection \<open>Functions\<close>
 
-abbreviation is_fun :: "[rel, set, set] \<Rightarrow> o" ("(_ : _ \<rightarrow> _)"[41, 0, 41] 40)
+abbreviation is_fun :: "[rel, set, set] \<Rightarrow> o" ("(_ : _ \<rightarrow> _)"[51, 0, 51] 50)
   where "f: A \<rightarrow> B \<equiv> f: A \<succ> B \<and> (\<forall>x \<in> A. \<exists>!y \<in> B. f(x, y))"
 
 abbreviation all_fun :: "[set, set, rel \<Rightarrow> o] \<Rightarrow> o"
@@ -186,6 +195,20 @@ proof
     using assms fun_image by simp
 qed
 
+text \<open>Injectivity and surjectivity of functions\<close>
+
+definition fun_inj :: "[rel, set, set] \<Rightarrow> o" ("(_ : _ \<rightarrow> _ injective)")
+  where "f: A \<rightarrow> B injective \<equiv> f: A \<rightarrow> B \<and> (\<forall>a \<in> A. \<forall>a' \<in> A. f[a] = f[a'] \<longrightarrow> a = a')"
+
+definition fun_surj :: "[rel, set, set] \<Rightarrow> o" ("(_ : _ \<rightarrow> _ surjective)")
+  where "f: A \<rightarrow> B surjective \<equiv> f: A \<rightarrow> B \<and> (\<forall>b \<in> B. \<exists>a \<in> A. f[a] = b)"
+
+lemma fun_inj_implies_rel_inj: "f: A \<rightarrow> B injective \<Longrightarrow> f: A \<succ> B injective"
+  sorry
+
+lemma rel_inj_fun_implies_fun_inj: "\<lbrakk>f: A \<rightarrow> B; f: A \<succ> B injective\<rbrakk> \<Longrightarrow> f: A \<rightarrow> B injective"
+  sorry
+
 
 section \<open>Axioms\<close>
 
@@ -200,10 +223,10 @@ A tabulation is a reflection of relations into sets.
 The third axiom states that tabulations exist.
 \<close>
 
-abbreviation tabulation_of :: "[set, rel, rel, rel, set, set] \<Rightarrow> o"
-  ("(_, _, _ tabulation'_of _ : _ \<succ> _)")
+abbreviation (input) is_tabulation_of :: "[set, rel, rel, rel, set, set] \<Rightarrow> o"
+  ("(_, _, _ is'_tabulation'_of _ : _ \<succ> _)")
 where
-  "T, f1, f2 tabulation_of \<phi>: A \<succ> B \<equiv>
+  "T, f1, f2 is_tabulation_of \<phi>: A \<succ> B \<equiv>
     f1: T \<rightarrow> A \<and>
     f2: T \<rightarrow> B \<and>
     (\<forall>x \<in> A. \<forall>y \<in> B. \<phi>(x, y) \<longleftrightarrow> (\<exists>t \<in> T. f1[t] = x \<and> f2[t] = y)) \<and>
@@ -214,7 +237,7 @@ axiomatization
   fst :: "rel \<Rightarrow> rel" ("|_|\<^sub>1") and
   snd :: "rel \<Rightarrow> rel" ("|_|\<^sub>2") where
 
-  tabulation: "\<forall>\<phi>: A \<succ> B. |\<phi>|, |\<phi>|\<^sub>1, |\<phi>|\<^sub>2 tabulation_of \<phi>: A \<succ> B"
+  tabulation: "\<forall>\<phi>: A \<succ> B. |\<phi>|, |\<phi>|\<^sub>1, |\<phi>|\<^sub>2 is_tabulation_of \<phi>: A \<succ> B"
 
 corollary fst_is_fun: "\<phi>: A \<succ> B \<Longrightarrow> |\<phi>|\<^sub>1: |\<phi>| \<rightarrow> A" using tabulation by auto
 
@@ -310,16 +333,17 @@ qed
 theorem singleton_exists: "\<exists>X. \<exists>x \<in> X. \<forall>y \<in> X. y = x"
 proof -
   from existence obtain a A where
-    a_in_A: "a \<in> A" by auto
+  a_in_A: "a \<in> A" by auto
 
-  from rel_comprehension[of A A "\<lambda>x y. x = a \<and> y = a"] obtain \<phi> where
-    \<phi>_rel: "\<phi>: A \<succ> A" and "\<forall>x \<in> A. \<forall>y \<in> A. \<phi>(x, y) \<longleftrightarrow> x = a \<and> y = a"
-      by auto
+  from rel_comprehension[of A A "\<lambda>x y. x = a \<and> y = a"]
+  obtain \<phi> where
+  \<phi>_rel: "\<phi>: A \<succ> A" and "\<forall>x \<in> A. \<forall>y \<in> A. \<phi>(x, y) \<longleftrightarrow> x = a \<and> y = a"
+    by auto
   with tabulation have
-    lemma_1: "\<forall>x \<in> A. \<forall>y \<in> A. x = a \<and> y = a \<longleftrightarrow> (\<exists>r \<in> |\<phi>|. |\<phi>|\<^sub>1[r] = x \<and> |\<phi>|\<^sub>2[r] = y)"
-      by auto
+  lemma_1: "\<forall>x \<in> A. \<forall>y \<in> A. x = a \<and> y = a \<longleftrightarrow> (\<exists>r \<in> |\<phi>|. |\<phi>|\<^sub>1[r] = x \<and> |\<phi>|\<^sub>2[r] = y)"
+    by auto
   then obtain r where
-    r_elem: "r \<in> |\<phi>|" and "|\<phi>|\<^sub>1[r] = a \<and> |\<phi>|\<^sub>2[r] = a"
+  r_elem: "r \<in> |\<phi>|" and "|\<phi>|\<^sub>1[r] = a \<and> |\<phi>|\<^sub>2[r] = a"
     using a_in_A by auto
 
   have "\<forall>r \<in> |\<phi>|. |\<phi>|\<^sub>1[r] = a \<and> |\<phi>|\<^sub>2[r] = a"
@@ -356,8 +380,8 @@ theorem emptyset_prop: "\<forall>x \<in> \<emptyset>. x \<notin> \<emptyset>"
 theorem vacuous: "\<forall>x \<in> \<emptyset>. P x"
 proof -
   { fix x assume assm: "x \<in> \<emptyset>"
-    hence "x \<notin> \<emptyset>" using emptyset_prop by auto
-    hence "P x" using assm by auto
+  hence "x \<notin> \<emptyset>" using emptyset_prop by auto
+  hence "P x" using assm by auto
   }
   thus ?thesis by auto
 qed
@@ -369,48 +393,161 @@ theorem singleton_prop: "\<exists>x \<in> \<one>. \<forall>y \<in> \<one>. y = x
 lemma singleton_all_eq: "\<forall>x \<in> \<one>. \<forall>y \<in> \<one>. x = y"
 proof -
   from singleton_prop obtain pt where lemma_1:
-    "\<forall>x \<in> \<one>. pt = x" by auto
+  "\<forall>x \<in> \<one>. pt = x" by auto
   { fix x y assume "x \<in> \<one>" and "y \<in> \<one>"
-    with lemma_1 have "pt = x" and "pt = y" by auto
-    hence "x = y" by simp
+  with lemma_1 have "pt = x" and "pt = y" by auto
+  hence "x = y" by simp
   }
   thus ?thesis by auto
 qed
 
+
+subsection \<open>The unique function into the singleton \<one>\<close>
+
 lemma singleton_terminal: "\<exists>\<exists>!f. f: X \<rightarrow> \<one>"
 proof
   from singleton_prop
-    obtain pt where
-    pt_elem: "pt \<in> \<one>"
+  obtain pt where
+  pt_elem: "pt \<in> \<one>"
     by auto
   from rel_comprehension[of X \<one> "\<lambda>_ y. y = pt"]
-    obtain f where
-    f_rel: "f: X \<succ> \<one>" and
-    f_def: "\<forall>x \<in> X. \<forall>y \<in> \<one>. f(x, y) \<longleftrightarrow> y = pt"
+  obtain f where
+  f_rel: "f: X \<succ> \<one>" and
+  f_def: "\<forall>x \<in> X. \<forall>y \<in> \<one>. f(x, y) \<longleftrightarrow> y = pt"
     by auto
   thus "\<exists>\<exists>f. f: X \<rightarrow> \<one>"
     using pt_elem by blast
 
   fix f g assume
-    f_fun: "f: X \<rightarrow> \<one>" and
-    g_fun: "g: X \<rightarrow> \<one>"
+  f_fun: "f: X \<rightarrow> \<one>" and
+  g_fun: "g: X \<rightarrow> \<one>"
   have
-    "\<forall>x \<in> X. f[x] = g[x]"
+  "\<forall>x \<in> X. f[x] = g[x]"
     using
       fun_image_elem_of[OF f_fun]
       fun_image_elem_of[OF g_fun]
       singleton_all_eq
     by auto
-  with fun_ext show
-    "f = g"
+  with fun_ext show "f = g"
     using f_fun g_fun by auto
 qed
+
+definition terminal :: "set \<Rightarrow> rel" ("(terminal\<^bsub>_\<^esub>)")
+  where "terminal\<^bsub>X\<^esub> \<equiv> \<iota>rel f: X \<succ> \<one> | (f: X \<rightarrow> \<one>)"
+
+lemma terminal_well_def: "terminal\<^bsub>X\<^esub>: X \<rightarrow> \<one>"
+  unfolding terminal_def
+  using
+    the_rel_sat[of X \<one> "\<lambda>f. f: X \<rightarrow> \<one>"]
+    singleton_terminal
+  by auto
+
+lemma terminal_constant: "\<forall>x \<in> X. \<forall>y \<in> X. terminal\<^bsub>X\<^esub>[x] = terminal\<^bsub>X\<^esub>[y]"
+  using
+    terminal_well_def[of X]
+    fun_image_elem_of
+    singleton_all_eq
+  by blast
 
 
 subsection \<open>Subsets\<close>
 
+text \<open>
+Differently from Shulman, we define subsets as tabulations satisfying certain properties.
+\<close>
 
+definition subset_of :: "[set, set] \<Rightarrow> o" (infix "\<subseteq>" 50)
+  where "B \<subseteq> A \<equiv>
+    \<exists>S: \<one> \<succ> A. \<exists>\<exists>i. (i: B \<rightarrow> A injective) \<and> (\<forall>pt \<in> \<one>. \<forall>a \<in> A. S(pt, a) \<longleftrightarrow> (\<exists>b \<in> B. i[b] = a))"
 
+lemma subsets_are_tabulations:
+  "B \<subseteq> A \<Longrightarrow> \<exists>S: \<one> \<succ> A. \<exists>i: B \<rightarrow> A. (B, terminal\<^bsub>B\<^esub>, i is_tabulation_of S: \<one> \<succ> A)" and
+  "\<exists>S: \<one> \<succ> A. \<exists>i: B \<rightarrow> A. (B, terminal\<^bsub>B\<^esub>, i is_tabulation_of S: \<one> \<succ> A) \<Longrightarrow> B \<subseteq> A"
+proof -
+  assume B_subset_of_A: "B \<subseteq> A"
+  then show "\<exists>S: \<one> \<succ> A. \<exists>i: B \<rightarrow> A. (B, terminal\<^bsub>B\<^esub>, i is_tabulation_of S: \<one> \<succ> A)"
+  proof -
+    have auxiliary_lemma:
+    "\<forall>i: B \<rightarrow> A. \<forall>pt \<in> \<one>. \<forall>a \<in> A. (\<exists>b \<in> B. i[b] = a) \<longleftrightarrow> (\<exists>b \<in> B. terminal\<^bsub>B\<^esub>[b] = pt \<and> i[b] = a)"
+    proof -
+      { fix i pt a assume
+      pt_elem: "pt \<in> \<one>"
+
+      have \<Longrightarrow>: "(\<exists>b \<in> B. i[b] = a) \<Longrightarrow> (\<exists>b \<in> B. terminal\<^bsub>B\<^esub>[b] = pt \<and> i[b] = a)"
+      proof -
+        assume a_in_range_of_i: "\<exists>b \<in> B. i[b] = a"
+        then obtain b where
+        b_elem: "b \<in> B" and
+        b_mapsto_a: "i[b] = a"
+          by auto
+        have
+        "terminal\<^bsub>B\<^esub>[b] = pt"
+          using
+            fun_image_elem_of[OF terminal_well_def b_elem]
+            pt_elem
+            singleton_all_eq
+          by auto
+        thus "\<exists>b \<in> B. terminal\<^bsub>B\<^esub>[b] = pt \<and> i[b] = a"
+          using b_elem b_mapsto_a by auto
+      qed
+      moreover have \<Longleftarrow>: "(\<exists>b \<in> B. terminal\<^bsub>B\<^esub>[b] = pt \<and> i[b] = a) \<Longrightarrow> (\<exists>b \<in> B. i[b] = a)"
+        by auto
+      ultimately have "(\<exists>b \<in> B. i[b] = a) \<longleftrightarrow> (\<exists>b \<in> B. terminal\<^bsub>B\<^esub>[b] = pt \<and> i[b] = a)" ..
+      }
+      thus ?thesis by auto
+    qed
+
+    from B_subset_of_A obtain S and i where
+    S_rel: "S: \<one> \<succ> A" and
+    i_inj: "i: B \<rightarrow> A injective" and
+    lemma_1: "\<forall>pt \<in> \<one>. \<forall>a \<in> A. S(pt, a) \<longleftrightarrow> (\<exists>b \<in> B. i[b] = a)"
+      unfolding subset_of_def by auto
+
+    have i_fun: "i: B \<rightarrow> A" by (fact conjunct1[OF i_inj[unfolded fun_inj_def]])
+
+    moreover have sufficiency:
+    "\<forall>pt \<in> \<one>. \<forall>a \<in> A. S(pt, a) \<longleftrightarrow> (\<exists>b \<in> B. terminal\<^bsub>B\<^esub>[b] = pt \<and> i[b] = a)"
+    proof -
+      { fix pt a assume
+      pt_elem: "pt \<in> \<one>" and
+      a_elem: "a \<in> A"
   
+      with lemma_1 have
+      "S(pt, a) \<longleftrightarrow> (\<exists>b \<in> B. i[b] = a)"
+        by auto
+      with auxiliary_lemma[rule_format, of i pt a] have
+      "S(pt, a) \<longleftrightarrow> (\<exists>b \<in> B. terminal\<^bsub>B\<^esub>[b] = pt \<and> i[b] = a)"
+        using i_fun pt_elem a_elem
+        by auto
+      }
+      thus ?thesis by auto
+    qed
+
+    moreover have minimality:
+    "\<forall>b \<in> B. \<forall>b' \<in> B. terminal\<^bsub>B\<^esub>[b] = terminal\<^bsub>B\<^esub>[b'] \<and> i[b] = i[b'] \<longrightarrow> b = b'"
+    proof -
+      { fix b b' assume
+      b_elem: "b \<in> B" and
+      b'_elem: "b' \<in> B"
+      then have "terminal\<^bsub>B\<^esub>[b] = terminal\<^bsub>B\<^esub>[b'] \<and> i[b] = i[b'] \<longrightarrow> b = b'"
+        using
+          conjunct2[OF i_inj[unfolded fun_inj_def]]
+          terminal_constant
+        by blast
+      }
+      thus ?thesis by auto
+    qed
+
+    ultimately have "B, terminal\<^bsub>B\<^esub>, i is_tabulation_of S: \<one> \<succ> A"
+      using terminal_well_def by auto
+
+    thus ?thesis using S_rel by auto
+  qed
+
+  next assume "\<exists>S: \<one> \<succ> A. \<exists>i: B \<rightarrow> A. (B, terminal\<^bsub>B\<^esub>, i is_tabulation_of S: \<one> \<succ> A)"
+  show "B \<subseteq> A" unfolding subset_of_def
+    sorry
+qed
+
 
 end
